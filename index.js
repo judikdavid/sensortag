3c4bed6c2db5fafff2c4868a6b7eecb84f4edf16
David Judik <judik.david@gmail.com>
2014-05-22 09:01:14 +0200

    add all sensor

--- index.js
+++ index.js
@@ -3,20 +3,50 @@ var SensorTag = require('sensortag');
 SensorTag.discover(function(sensorTag){
   console.log(sensorTag.toString());
   sensorTag.connect(function(){
-    console.log('connect');
     sensorTag.discoverServicesAndCharacteristics(function(callback){
       console.log('set');
-      sensorTag.enableIrTemperature();
-      //device.readIrTemperature(logTemp);
-      sensorTag.notifyIrTemperature(logTemp);
-      sensorTag.on('irTemperatureChange',logTemp);
-      //device.disableIrTemperature();
+      //sensorTag.enableIrTemperature();
+      //sensorTag.notifyIrTemperature(logTemp);
+      //sensorTag.on('irTemperatureChange',logTemp);
+      //sensorTag.enableGyroscope();
+      //sensorTag.notifyGyroscope(logGyro);
+      //sensorTag.on('gyroscopeChange',logGyro);
+      //sensorTag.enableAccelerometer();
+      //sensorTag.notifyAccelerometer(logAccelero);
+      //sensorTag.on('accelerometerChange',logAccelero);
+      sensorTag.enableBarometricPressure();
+      sensorTag.notifyBarometricPressure(logBaro);
+      sensorTag.on('barometricPressureChange',logBaro);
+      //sensorTag.notifySimpleKey(logKey);
+      //sensorTag.on('simpleKeyChange', logKey);
+      //sensorTag.enableHumidity();
+      //sensorTag.notifyHumidity(logHumi);
+      //sensorTag.on('humidityChange',logHumi);
     });
 
-    function logTemp(objectTemperature,ambientTemperature)
-    {
+    function logTemp(objectTemperature,ambientTemperature){
       if(objectTemperature!=undefined) console.log('readIrTemperature: objectTemperature='+objectTemperature);
       if(ambientTemperature!=undefined) console.log('readIrTemperature: ambientTemperature='+ambientTemperature);
+    }
+
+    function logGyro(x, y, z){
+      console.log(x, y, z);
+    }
+
+    function logAccelero(x, y, z){
+      console.log(x, y, z);
+    }
+
+    function logBaro(pressure){
+      console.log(pressure);
+    }
+
+    function logKey(left, right){
+      console.log(left, right); 
+    }
+
+    function logHumi(temperature, humidity){
+      console.log(temperature, humidity); 
     }
   });
 });
